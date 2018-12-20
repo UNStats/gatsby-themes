8293bb1756b409c608a960e50f392ceb6a85466d
Thilo Maier <git@maier.asia>
2018-12-19 22:01:09 -0500

    Add config files for a very basic Gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: "United Nations World Data Forum",
+    description:
+      "The 3rd UN World Data Forum will take place 18-21 October 2020 in Bern, Switzerland.",
+    url: "https://next.undataforum.org"
+  }
+};
