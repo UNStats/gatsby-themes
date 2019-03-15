9f90971c6e88f044cb1561f836585e4eb9d97b9b
Thilo Maier <git@maier.asia>
2019-03-15 17:57:05 +0000

    Rename theme in docs

--- examples/docs/gatsby-config.js
+++ examples/docs/gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  __experimentalThemes: ["@undataforum/gatsby-theme-main"]
+  siteMetadata: {
+    title: "UN World Data Forum Docs",
+    siteUrl: "https://docs.undataforum.org",
+    description: "Technical documentation for UN World Data Forum websites.",
+    navigation: [{ href: "/about/", text: "About" }]
+  },
+  __experimentalThemes: ["gatsby-theme-undataforum"]
 };
