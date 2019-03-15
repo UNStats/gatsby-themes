c0b85016d1b1d99dd14055eb54435fb65b8b1d71
Thilo Maier <git@maier.asia>
2019-03-15 17:55:29 +0000

    Ranme folder for main theme

--- themes/gatsby-theme-undataforum/gatsby-config.js
+++ themes/gatsby-theme-undataforum/gatsby-config.js
@@ -1,24 +1,8 @@
 const pkg = require("./package.json");
 
 module.exports = {
-  siteMetadata: {
-    title: pkg.name,
-    description: pkg.description,
-    siteUrl: "https://theme.undataforum.org",
-    navigation: [
-      {
-        href: "/about",
-        text: "About"
-      },
-      {
-        href: "/committee",
-        text: "Committee"
-      }
-    ]
-  },
   plugins: [
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-styled-components",
     // Install "gatsby-plugin-page-creator" if the theme should be able to generate pages.
     // {
     //   resolve: "gatsby-plugin-page-creator",
@@ -34,6 +18,9 @@ module.exports = {
           default: require.resolve("./src/components/ContainerLayout.jsx")
         }
       }
-    }
+    },
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-styled-components"
   ]
 };
---
+++ themes/gatsby-theme-undataforum/src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Flex } from "@undataforum/components";
+import Img from "gatsby-image";
+import styled from "styled-components";
+
+const BackgroundImage = styled(Img)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: -1;
+  height: ${props => props.height || "100vh"};
+`;
+
+export default ({ children, ...props }) => (
+  <>
+    <Flex
+      css={`
+        position: relative;
+      `}
+      alignItems="center"
+    >
+      <BackgroundImage {...props} />
+    </Flex>
+  </>
+);
