45c47947347a1edd4f719b3943d502bb068c394f
Thilo Maier <git@maier.asia>
2019-05-27 22:32:12 -0400

    Use logo from @undataforum/tokens

--- themes/gatsby-theme-undataforum/src/components/index.js
+++ themes/gatsby-theme-undataforum/src/components/index.js
@@ -2,7 +2,6 @@ export { default as BlogPreview } from './BlogPreview';
 export { default as Header } from './Header';
 export { default as Hero } from './Hero';
 export { default as Link } from './Link';
-export { default as Logo } from './Logo';
 export { default as PostList } from './PostList';
 export { default as ProfilePreview } from './ProfilePreview';
 export { default as ProfileList } from './ProfileList';
