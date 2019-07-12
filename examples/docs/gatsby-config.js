13c4bf2c97002b6ca32154c07bbbcfdbaf890a72
Thilo Maier <git@maier.asia>
2019-07-12 14:58:21 -0400

    docs: Remove obsolete docs

--- examples/docs/gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'UN World Data Forum Docs',
-    siteUrl: 'https://docs.undataforum.org',
-    description: 'Technical documentation for UN World Data Forum websites.',
-    navigation: [{ href: '/about/', text: 'About' }],
-  },
-  __experimentalThemes: ['gatsby-theme-undataforum'],
-};
