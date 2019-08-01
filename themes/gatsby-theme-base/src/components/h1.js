0941b8d6488e7fd1408a338518d2c122004762bb
Thilo Maier <git@maier.asia>
2019-08-01 12:17:30 -0400

    fix(base): Fix bottom margins in headings (close #72)

--- themes/gatsby-theme-base/src/components/h1.js
+++ themes/gatsby-theme-base/src/components/h1.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Heading } from '@undataforum/components';
 
+// Add m={0} to Heading due to https://github.com/UNDataForum/gatsby-themes/issues/72.
 const H1 = ({ ...props }) => (
-  <Heading {...props} as="h1" fontSize={5} lineHeight="title" mb={4} />
+  <Heading {...props} as="h1" fontSize={5} lineHeight="title" m={0} mb={4} />
 );
 
 export default H1;
