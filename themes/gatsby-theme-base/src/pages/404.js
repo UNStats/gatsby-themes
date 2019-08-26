8cff0bed0e9878d407f235d0a303235bf7cca461
Thilo Maier <git@maier.asia>
2019-08-26 17:16:03 -0400

    feat: Add default 404 page (closes #39)

---
+++ themes/gatsby-theme-base/src/pages/404.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shape, string } from 'prop-types';
+import { Text } from '@undataforum/components';
+import DefaultPage from '../components/default-page';
+
+const NotFound = ({ location }) => (
+  <DefaultPage title="Page not found" location={location}>
+    <Text fontFamily="body">
+      Oops! The page you are looking for has been removed or relocated.
+    </Text>
+  </DefaultPage>
+);
+
+NotFound.propTypes = {
+  location: shape({ pathname: string.isRequired }).isRequired,
+};
+
+export default NotFound;
--- themes/gatsby-theme-base/src/templates/default.js
+++ themes/gatsby-theme-base/src/templates/default.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { node, shape, string } from 'prop-types';
 import DefaultPage from '../components/default-page';
 
+// The default template is for MDX pages only.
+// It does the additional step of reading title and description from frontmatter.
 const DefaultTemplate = ({ pageContext, location, children }) => {
   const { title, description } = pageContext.frontmatter;
   return (
