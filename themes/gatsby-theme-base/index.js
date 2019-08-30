121e3416a744d55df639c687c079f0f6a09a6b9c
Thilo Maier <git@maier.asia>
2019-08-30 16:39:41 -0400

    feat: Export Header

--- themes/gatsby-theme-base/index.js
+++ themes/gatsby-theme-base/index.js
@@ -1,4 +1,5 @@
 export { default as H1 } from './src/components/h1';
+export { default as Header } from './src/components/header';
 export { default as Layout } from './src/components/layout';
 export { default as Seo } from './src/components/seo';
 export { default as MDXRenderer } from 'gatsby-plugin-mdx/mdx-renderer';
