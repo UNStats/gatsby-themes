61fd92cb71249e578197b5be2e437afa51105470
Thilo Maier <git@maier.asia>
2019-10-31 15:16:27 -0400

    Do not export header and footer

--- themes/gatsby-theme-base/index.js
+++ themes/gatsby-theme-base/index.js
@@ -1,4 +1,3 @@
-export { default as Header } from './src/components/header';
 export { default as Layout } from './src/components/layout';
 export { default as MDXRenderer } from 'gatsby-plugin-mdx/mdx-renderer';
 export { Styled } from 'theme-ui';
