7d63243b3071a0213ed11e05fb9abac9e268e177
Thilo Maier <git@maier.asia>
2019-12-18 22:18:59 -0500

    Add About component prototype GH-260

---
+++ site/src/components/about.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container, Text } from '@undataforum/components';
+
+const About = () => (
+  <Container
+    sx={{
+      maxWidth: 'width.narrow',
+      fontSize: 3,
+      textAlign: 'center',
+      px: [2, 3, 4],
+      mb: [4, null, 5],
+    }}
+  >
+    <Text>
+      Tempor nisi veniam deserunt qui deserunt. Nostrud deserunt deserunt aliqua
+      ipsum minim irure. Deserunt deserunt mollit consequat do excepteur culpa
+      reprehenderit elit. Excepteur ut id ea ea dolor minim non veniam ea
+      exercitation cupidatat.
+    </Text>
+  </Container>
+);
+
+export default About;
