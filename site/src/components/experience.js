7716178d29ff4094e395a5d7b0c67b32017017d4
Thilo Maier <git@maier.asia>
2019-12-23 13:56:50 -0500

    Use color boxes for Experience component GH-272

--- site/src/components/experience.js
+++ site/src/components/experience.js
@@ -3,26 +3,34 @@ import { Box, Container, Grid, Text } from '@undataforum/components';
 
 const Experience = ({ ...props }) => (
   <Container {...props} sx={{ maxWidth: 'width.wide', px: [2, 3, 4] }}>
-    <Grid gap={[3, 4]} columns={[1, 2, 4]}>
-      <Box>
+    <Grid gap={3} columns={[1, 2, 4]}>
+      <Box
+        sx={{ textAlign: ['center', 'left'], p: 3, variant: 'pairings.green' }}
+      >
         <Text sx={{ fontSize: 5, fontWeight: 'heading' }}>Labore</Text>
         <Text>
           Eiusmod minim aliqua elit magna cupidatat nulla non eiusmod eiusmod
           elit.
         </Text>
       </Box>
-      <Box>
+      <Box
+        sx={{ textAlign: ['center', 'left'], p: 3, variant: 'pairings.blue' }}
+      >
         <Text sx={{ fontSize: 5, fontWeight: 'heading' }}>Irure</Text>
         <Text>
           Ipsum incididunt ut cillum amet nostrud et aliquip officia sint
           consequat officia labore.
         </Text>
       </Box>
-      <Box>
-        <Text sx={{ fontSize: 5, fontWeight: 'heading' }}>Reprehendere</Text>
+      <Box
+        sx={{ textAlign: ['center', 'left'], p: 3, variant: 'pairings.red' }}
+      >
+        <Text sx={{ fontSize: 5, fontWeight: 'heading' }}>Clamare</Text>
         <Text>Magna pariatur in ipsum officia velit voluptate proident.</Text>
       </Box>
-      <Box>
+      <Box
+        sx={{ textAlign: ['center', 'left'], p: 3, variant: 'pairings.yellow' }}
+      >
         <Text sx={{ fontSize: 5, fontWeight: 'heading' }}>Consequare</Text>
         <Text>
           Voluptate dolore ut deserunt mollit occaecat cupidatat occaecat
