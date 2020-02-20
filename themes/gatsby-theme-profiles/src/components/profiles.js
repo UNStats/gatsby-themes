36df9e0e4b8c5f843154c9cc8590da332d9a8a45
Thilo Maier <git@maier.asia>
2020-02-20 15:50:05 -0500

    Localize title and description of profiles overview page GH-348

--- themes/gatsby-theme-profiles/src/components/profiles.js
+++ themes/gatsby-theme-profiles/src/components/profiles.js
@@ -4,56 +4,86 @@ import { Container, Grid, Styled } from 'theme-ui';
 import { ProfilePreview, SmartLink } from '@undataforum/components';
 import { Layout } from '@undataforum/gatsby-theme-base';
 import Img from 'gatsby-image';
+import {
+  createIntl,
+  createIntlCache,
+  FormattedMessage,
+  RawIntlProvider,
+} from 'react-intl';
 
-const Profiles = ({ blurb, data, pageContext: { collection }, location }) => {
+import messages from '../i18n/messages';
+
+const Profiles = ({
+  blurb,
+  data,
+  pageContext: { collection, lang },
+  location,
+}) => {
   const profiles = data.allProfile.nodes;
+  // We need to localize props that are not React components:
+  // https://github.com/formatjs/react-intl/blob/master/docs/API.md#createintl
+  const cache = createIntlCache();
+  const intl = createIntl(
+    {
+      locale: lang,
+      messages: messages[lang],
+    },
+    cache
+  );
   return (
     // eslint-disable-next-line no-warning-comments
-    // TODO Use collection as key to localize title and description.
-    // eslint-disable-next-line no-warning-comments
     // TODO Add page description (use SEO component).
-    <Layout location={location} title={collection}>
-      <Container sx={{ maxWidth: 'width.default', px: [2, 3, 4] }}>
-        <Styled.h1>{collection}</Styled.h1>
-        {blurb}
-        <Grid gap={4} columns={[1, 2, 3]}>
-          {profiles.map(({ id, path, ...profile }) => {
-            const {
-              avatar: {
-                childImageSharp: { fixed },
-              },
-              honorific,
-              name,
-              jobtitle,
-              organization,
-            } = profile;
-            return (
-              <SmartLink
-                css={{ ':hover': { textDecoration: 'none' } }}
-                href={path}
-                key={id}
-              >
-                <ProfilePreview
-                  profile={{
-                    avatar: (
-                      <Img
-                        style={{ borderRadius: '100%' }}
-                        alt={name}
-                        fixed={fixed}
-                      />
-                    ),
-                    honorific,
-                    name,
-                    jobtitle,
-                    organization,
-                  }}
-                />
-              </SmartLink>
-            );
-          })}
-        </Grid>
-      </Container>
-    </Layout>
+    // We would normally use `IntlProvider`, but we already have `intl` and therefore reuse it with RawIntlProvider.
+    <RawIntlProvider value={intl}>
+      <Layout
+        title={intl.formatMessage({ id: `${collection}.title` })}
+        description={intl.formatMessage({ id: `${collection}.description` })}
+        location={location}
+      >
+        <Container sx={{ maxWidth: 'width.default', px: [2, 3, 4] }}>
+          <Styled.h1>
+            <FormattedMessage id="profiles.title" />
+          </Styled.h1>
+          {blurb}
+          <Grid gap={4} columns={[1, 2, 3]}>
+            {profiles.map(({ id, path, ...profile }) => {
+              const {
+                avatar: {
+                  childImageSharp: { fixed },
+                },
+                honorific,
+                name,
+                jobtitle,
+                organization,
+              } = profile;
+              return (
+                <SmartLink
+                  css={{ ':hover': { textDecoration: 'none' } }}
+                  href={path}
+                  key={id}
+                >
+                  <ProfilePreview
+                    profile={{
+                      avatar: (
+                        <Img
+                          style={{ borderRadius: '100%' }}
+                          alt={name}
+                          fixed={fixed}
+                        />
+                      ),
+                      honorific,
+                      name,
+                      jobtitle,
+                      organization,
+                    }}
+                  />
+                </SmartLink>
+              );
+            })}
+          </Grid>
+        </Container>
+      </Layout>
+    </RawIntlProvider>
   );
 };
 
---
+++ themes/gatsby-theme-profiles/src/i18n/translations/en.js
@@ -0,0 +1,7 @@
+const en = {
+  'profiles.title': 'Profiles',
+  'profiles.description':
+    'Description for overview page of profiles collection.',
+};
+
+export default en;
