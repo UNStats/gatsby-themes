029214bca65c444e5172dea0e871d369c01e8448
Thilo Maier <git@maier.asia>
2020-09-29 20:49:19 +0000

    Deep merge without deepmerge package Fixes #549

--- microfrontends/profiles/src/@undataforum/gatsby-theme-profiles/i18n/translations/en.js
+++ microfrontends/profiles/src/@undataforum/gatsby-theme-profiles/i18n/translations/en.js
@@ -1,11 +1,11 @@
-import defaults from '@undataforum/gatsby-theme-profiles/src/i18n/translations/en';
-import merge from 'deepmerge';
+import defaultTranslations from '@undataforum/gatsby-theme-profiles/src/i18n/translations/en';
 
 // Shadow default localizations.
-const en = merge(defaults, {
+const translations = {
+  ...defaultTranslations,
   // Shadow title and description for profiles overview page for `profiles` collection.
   'profiles.title': 'Shadowed profiles overview page title',
   'profiles.description': 'Shadowed profiles overview page description.',
-});
+};
 
-export default en;
+export default translations;
